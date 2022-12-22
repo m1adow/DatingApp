ab25ef6a9af2f2d939977431bcfd0e0961310b8f
Valentin <84251113+m1adow@users.noreply.github.com>
2022-12-22 23:32:09 +0200

    init

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
+		{
+            this.context = context;
+        }
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await this.context.Users.ToListAsync();
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<AppUser>> GetUser(int id) => await context.Users.FindAsync(id);
+	}
+}
