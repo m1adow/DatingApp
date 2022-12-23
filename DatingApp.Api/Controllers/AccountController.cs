d401c2897103db4e0fc82f7056d7051ee62bc9dd
Valentin <84251113+m1adow@users.noreply.github.com>
2022-12-24 00:48:37 +0200

    added dto and register

---
+++ DatingApp.Api/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using DatingApp.Api.Data;
+using DatingApp.Api.DTOs;
+using DatingApp.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Controllers
+{
+	public class AccountController : BaseApiController
+	{
+        private readonly DataContext context;
+
+        public AccountController(DataContext context)
+		{
+            this.context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> RegisterAsync(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.UserName))
+                return BadRequest("Username is taken");
+
+            using (var hmac = new HMACSHA512())
+            {
+                var user = new AppUser
+                {
+                    UserName = registerDto.UserName.ToLower(),
+                    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                    PasswordSalt = hmac.Key
+                };
+
+                await this.context.AddAsync(user);
+                await this.context.SaveChangesAsync();
+
+                return user;
+            }
+        }
+
+        private async Task<bool> UserExists(string userName) => await this.context.Users.AnyAsync(x => x.UserName == userName.ToLower());
+	}
+}
+
--- DatingApp.Api/Data/Migrations/20221223210901_UserPasswordsAdded.Designer.cs
+++ DatingApp.Api/Data/Migrations/20221223210901_UserPasswordsAdded.Designer.cs
@@ -2,6 +2,7 @@
 using DatingApp.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.Api.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221223210901_UserPasswordsAdded")]
+    partial class UserPasswordsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.1");
