2b6e7384ffbd6bc4342fe5f36292be8cd503637f
Valentin <84251113+m1adow@users.noreply.github.com>
2023-01-06 22:14:27 +0200

    created userlike entity

--- DatingApp.Api/Entities/AppUser.cs
+++ DatingApp.Api/Entities/AppUser.cs
@@ -19,6 +19,9 @@ namespace DatingApp.Api.Entities
 		public string City { get; set; }
 		public string Country { get; set; }
 		public List<Photo> Photos { get; set; } = new();
+
+		public List<UserLike> LikedByUsers { get; set; }
+		public List<UserLike> LikedUsers { get; set; }
 	}
 }
 
---
+++ DatingApp.Api/Entities/UserLike.cs
@@ -0,0 +1,11 @@
+﻿namespace DatingApp.Api.Entities
+{
+	public class UserLike
+	{
+		public AppUser SourceUser { get; set; }
+		public int SourceUserId { get; set; }
+		public AppUser TargerUser { get; set; }
+		public int TargetUserId { get; set; }
+	}
+}
+
