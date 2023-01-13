5b9925f03528a9badfb592ad84b50c25d76f81f8
Valentin <84251113+m1adow@users.noreply.github.com>
2023-01-13 20:00:02 +0200

    added benefits to admins

---
+++ DatingApp.Api/Controllers/AdminController.cs
@@ -0,0 +1,80 @@
+﻿using DatingApp.Api.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Controllers
+{
+	public class AdminController : BaseApiController
+	{
+		private readonly UserManager<AppUser> userManager;
+
+		public AdminController(UserManager<AppUser> userManager)
+		{
+			this.userManager = userManager;
+		}
+
+		[Authorize(Policy = "RequireAdminRole")]
+		[HttpGet("users-with-roles")]
+		public async Task<ActionResult> GetUsersWithRolesAsync()
+		{
+			var users = await this.userManager.Users
+											  .OrderBy(u => u.UserName)
+											  .Select(u => new
+											  {
+												  u.Id,
+												  UserName = u.UserName,
+												  Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+											  })
+											  .ToListAsync();
+
+			return Ok(users);
+		}
+
+		[Authorize(Policy = "RequireAdminRole")]
+		[HttpPost("edit-roles/{username}")]
+		public async Task<ActionResult> EditRolesAsync(string username, [FromQuery] string roles)
+		{
+			if (string.IsNullOrEmpty(roles))
+			{
+				return BadRequest("You must select at least one role");
+			}
+
+			var selectedRoles = roles.Split(",");
+
+			var user = await this.userManager.FindByNameAsync(username);
+
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			var userRoles = await this.userManager.GetRolesAsync(user);
+
+			var result = await this.userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+			if (!result.Succeeded)
+			{
+				return BadRequest("Failed to add to roles");
+			}
+
+			result = await this.userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Failed to remove from roles");
+            }
+
+			return Ok(await this.userManager.GetRolesAsync(user));
+        }
+
+		[Authorize(Policy = "ModeratePhotoRole")]
+		[HttpGet("photos-to-moderate")]
+		public ActionResult GetPhotoForModeration()
+		{
+			return Ok("Admins or moderators can see this");
+		}
+	}
+}
+
