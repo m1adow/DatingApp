c2130440ede020f8d87b3a0da82cc22bd55f4ae9
Valentin <84251113+m1adow@users.noreply.github.com>
2023-01-17 19:49:27 +0200

    started adding live chatting

---
+++ DatingApp.Api/SignalR/MessageHub.cs
@@ -0,0 +1,40 @@
+﻿using DatingApp.Api.Extensions;
+using DatingApp.Api.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DatingApp.Api.SignalR
+{
+	public class MessageHub : Hub
+	{
+        private readonly IMessageRepository messageRepository;
+
+        public MessageHub(IMessageRepository messageRepository)
+		{
+            this.messageRepository = messageRepository;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext.Request.Query["user"];
+            var groupName = GetGroupName(Context.User.GetUserName(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await this.messageRepository.GetMessageThreadAsync(Context.User.GetUserName(), otherUser);
+
+            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private string GetGroupName(string caller, string other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+    }
+}
+
